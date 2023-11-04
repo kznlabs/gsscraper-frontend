5ff249554c797e58454faa132b67b12e4fd790ec
investintesla <kaamil3q@gmail.com>
2023-11-04 01:12:45 +0100

    Beta fjuczers

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,70 +1,136 @@
 import React from 'react';
 import styles from '../styles/Table.module.css';
-import Link from "next/link"; // Import CSS module
+import Link from "next/link";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"; // Import CSS module
+import {faCircleInfo} from '@fortawesome/free-solid-svg-icons'
+import { Tooltip } from 'react-tooltip'
+
+function returnTooltip({produkt}) {
+
+    return (
+        <>
+            `<div className={styles.hotshotPrice}>
+                <h3>Cena z GS:</h3>{produkt['price']}
+            </div>
+
+            <div className={styles.lastPrice}>
+                <h3>Cena przed obniżką:</h3>{produkt['price']}
+            </div>
+
+            <div className={styles.lowestPrice}>
+                <h3>Cena z ostatnich 30 dni:</h3>{produkt['price']}
+
+            </div>`
+        </>
+    );
+}
 
 function Table({data}) {
     return (
-        <div className={styles.productTableContainer}>
-            <table className={styles.productTable}> {/* Apply the CSS class */}
-                <thead>
-                <tr>
-                    <th>Nazwa produktu</th>
-                    <th>Kod x-kom'u</th>
-                    <th>Cena</th>
-                    <th>Stara cena</th>
-                    <th>Cena z 30 dni</th>
-                    <th>Ilość</th>
-                    <th>Zdjęcie produktu</th>
-                    <th>Data</th>
-                </tr>
-                </thead>
-                <tbody>
-                {data.map((product) => (
-                    <tr key={product.id}>
-                        <td className={styles.productName}>
-                            {product.itemName}
-                        </td>
+        // <div className="">
+        //     <div className={styles.row}>
+        //         <div className={styles.column}>Nazwa produktu</div>
+        //         <div className={styles.column}>Kod x-kom'u</div>
+        //         <div className={styles.column}>Cena</div>
+        //         <div className={styles.column}>Stara cena</div>
+        //         <div className={styles.column}>Cena z 30 dni</div>
+        //         <div className={styles.column}>Ilość</div>
+        //         <div className={styles.column}>Zdjęcie produktu</div>
+        //         <div className={styles.column}>Data</div>
+        //     </div>
+        //     {data.map((product) => (
+        //         <div className={styles.row} key={product.id}>
+        //             <div className={styles.productName}>
+        //                 {product.itemName}
+        //             </div>
+        //
+        //             <div>
+        //                 <div className={styles.hideOnFullWidth}>Kod x-kom'u:</div>
+        //                 <Link href={'https://x-kom.pl/p/'+product.sku}>{product.sku}</Link>
+        //             </div>
+        //
+        //             <div className={styles.hideOnMobileWidth}>
+        //                 <div className={styles.hideOnFullWidth}>Cena:</div>
+        //                 {product.price} zł
+        //             </div>
+        //
+        //             <div>
+        //                 <div className={styles.hideOnFullWidth}>Stara cena:</div>
+        //                 {product.oldPrice} zł
+        //             </div>
+        //
+        //             <div>
+        //                 <div className={styles.hideOnFullWidth}>Najniższa cena z 30 dni:</div>
+        //                 {product.minPrice} zł
+        //             </div>
+        //
+        //             <div>
+        //                 <div className={styles.hideOnFullWidth}>Ilość sztuk:</div>
+        //                 {product.amount} szt.
+        //             </div>
+        //
+        //             <div className={styles.productThumbnail}>
+        //                 <img src={product.thumbnailUrl}
+        //                      alt={`Miniaturka Gorącego strzału - ${product.itemName} - ${product.promotionStart}`}/>
+        //             </div>
+        //
+        //             <div className={styles.footer}>
+        //                 <div className={styles.hideOnFullWidth}>Data:</div>
+        //                 {product.promotionStart}
+        //             </div>
+        //         </div>
+        //     ))}
+        //
+        // </div>
+        <div className={styles.hotshots_list}>
+            <div className={styles.row}>
+                <div className={styles.col}>Nazwa produktu</div>
+                <div className={styles.col}>Kod x-kom'u</div>
+                <div className={styles.col}>Ceny</div>
+                <div className={styles.col}>Ilość</div>
+                <div className={styles.col}>Zdjęcie produktu</div>
+                <div className={styles.col}>Data</div>
+            </div>
+            {data.map((product) => (
+                <div className={styles.row} key={product.id}>
+                    <div className={styles.col}>
+                        {product.itemName}
+                    </div>
 
-                        <td>
-                            <div className={styles.hideOnFullWidth}>Kod x-kom'u:</div>
-                            <Link href={'https://x-kom.pl/p/'+product.sku}>{product.sku}</Link>
-                        </td>
+                    <div className={styles.col}>
+                        <Link href={'https://x-kom.pl/p/'+product.sku}>{product.sku}</Link>
+                    </div>
 
-                        <td className={styles.hideOnMobileWidth}>
-                            <div className={styles.hideOnFullWidth}>Cena:</div>
-                            {product.price} zł
-                        </td>
 
-                        <td>
-                            <div className={styles.hideOnFullWidth}>Stara cena:</div>
-                            {product.oldPrice} zł
-                        </td>
+                    <div className={styles.col}>
+                        {product.price} zł
 
-                        <td>
-                            <div className={styles.hideOnFullWidth}>Najniższa cena z 30 dni:</div>
-                            {product.minPrice} zł
-                        </td>
+                        <a data-tooltip-id="price-tooltip"
+                           data-tooltip-html={'Cena z ostatnich 30 dni: ' + product.minPrice + ' zł<br>Cena przed obnizką: ' + product.oldPrice + ' zł'}
+                        >
 
-                        <td>
-                            <div className={styles.hideOnFullWidth}>Ilość sztuk:</div>
-                            {product.amount} szt.
-                        </td>
+                            <FontAwesomeIcon icon={faCircleInfo}/>
+                        </a>
 
-                        <td className={styles.productThumbnail}>
-                            <img src={product.thumbnailUrl}
-                                 alt={`Miniaturka Gorącego strzału - ${product.itemName} - ${product.promotionStart}`}/>
-                        </td>
+                        <Tooltip id="price-tooltip" />
+                    </div>
 
-                        <td className={styles.footer}>
-                            <div className={styles.hideOnFullWidth}>Data:</div>
-                            {product.promotionStart}
-                        </td>
-                    </tr>
-                ))}
-                </tbody>
+                    <div className={styles.col}>
+                        {product.amount}
+                    </div>
 
-            </table>
+                    <div className={[styles.col, styles.productImage].join(' ')}>
+                        <img src={product.thumbnailUrl}
+                                             alt={`Miniaturka Gorącego strzału - ${product.itemName} - ${product.promotionStart}`}/>
+                    </div>
+
+                    <div className={styles.col}>
+                        {product.promotionStart}
+                    </div>
+                </div>
+            ))}
         </div>
+
     );
 }
 
--- src/pages/api.js
+++ src/pages/api.js
@@ -21,7 +21,12 @@ export const fetchAndFormatData = async (page, itemsPerPage) => {
 
 export const formatTime = (timestamp) => {
     const date = new Date(timestamp);
-    const hours = date.getHours().toString().padStart(2, '0');
-    const minutes = date.getMinutes().toString().padStart(2, '0');
-    return `${date.getDate()}-${date.getMonth().toString().padStart(2, '0')}-${date.getFullYear()} ${hours}:${minutes}`;
+    const options = {
+        year: 'numeric',
+        month: '2-digit',
+        day: '2-digit',
+        hour: '2-digit',
+        minute: '2-digit'
+    };
+    return date.toLocaleString('pl', options);
 };
